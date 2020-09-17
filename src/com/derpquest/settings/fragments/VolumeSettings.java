7b6e438505561280ca3cdf0fc82358124e23434e
TikkiX2 <radstylemusic@gmail.com>
2020-09-17 03:29:30 -0700

    Add Volume Panel Background type [2/3]

--- src/com/derpquest/settings/fragments/VolumeSettings.java
+++ src/com/derpquest/settings/fragments/VolumeSettings.java
@@ -41,6 +41,8 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,8 +52,15 @@ public class VolumeSettings extends SettingsPreferenceFragment
 
     private ListPreference mVolumePanelTheme;
     private ListPreference mVolumeAlignment;
+    private ColorPickerPreference mColor;
+    private Preference mImage;
+    private ListPreference mBackgroundTypeList;
     private static final String SYSTEMUI_PLUGIN_VOLUME = "systemui_plugin_volume";
     private static final String VOLUME_PANEL_ALIGNMENT = "volume_panel_alignment";
+    private static final String SYNTHOS_VOLUME_PANEL_BACKGROUND_COLOR = "synthos_volume_panel_background_color";
+    private static final String SYNTHOS_VOLUME_PANEL_BACKGROUND_TYPE = "synthos_volume_panel_background_type";
+    private static final String FILE_VOLUME_SELECT = "file_volume_select";
+    private static final int REQUEST_PICK_IMAGE = 0;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -77,6 +86,46 @@ public class VolumeSettings extends SettingsPreferenceFragment
         mVolumeAlignment.setValue(String.valueOf(align));
         mVolumeAlignment.setSummary(mVolumeAlignment.getEntry());
         mVolumeAlignment.setOnPreferenceChangeListener(this);
+
+        // set volume background color
+        mColor = (ColorPickerPreference) findPreference(SYNTHOS_VOLUME_PANEL_BACKGROUND_COLOR);
+        mColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getIntForUser(resolver,
+                Settings.System.SYNTHOS_VOLUME_PANEL_BACKGROUND_COLOR, 0xffffffff, UserHandle.USER_CURRENT);
+        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mColor.setSummary(hexColor);
+        mColor.setNewPreviewColor(intColor);
+
+        mBackgroundTypeList = (ListPreference) findPreference(SYNTHOS_VOLUME_PANEL_BACKGROUND_TYPE);
+        int bgType = Settings.System.getInt(resolver,
+                Settings.System.SYNTHOS_VOLUME_PANEL_BACKGROUND_TYPE, 1);
+        mBackgroundTypeList.setValue(String.valueOf(bgType));
+        mBackgroundTypeList.setSummary(mBackgroundTypeList.getEntry());
+        mBackgroundTypeList.setOnPreferenceChangeListener(this);
+
+        mImage = findPreference(FILE_VOLUME_SELECT);
+
+        setEnabled();
+
+    }
+
+    private void setEnabled() {
+        final ContentResolver resolver = getContentResolver();
+        int backgroundType = Settings.System.getInt(resolver,
+                Settings.System.SYNTHOS_VOLUME_PANEL_BACKGROUND_TYPE, 0);
+        mColor.setEnabled(backgroundType == 2);
+        mImage.setEnabled(backgroundType == 3);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mImage) {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent, REQUEST_PICK_IMAGE);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 
     @Override
@@ -100,8 +149,35 @@ public class VolumeSettings extends SettingsPreferenceFragment
                     Settings.System.VOLUME_PANEL_ALIGNMENT, align);
             mVolumeAlignment.setSummary(mVolumeAlignment.getEntries()[index]);
             return true;
+        } else if (preference == mColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.SYNTHOS_VOLUME_PANEL_BACKGROUND_COLOR, intHex, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mBackgroundTypeList) {
+            int type = Integer.valueOf((String) newValue);
+            int index = mBackgroundTypeList.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SYNTHOS_VOLUME_PANEL_BACKGROUND_TYPE, type);
+            mBackgroundTypeList.setSummary(mBackgroundTypeList.getEntries()[index]);
+            setEnabled();
+            return true;
         }
         return false;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (requestCode == REQUEST_PICK_IMAGE) {
+            if (resultCode != Activity.RESULT_OK) {
+                return;
+            }
+            final Uri imageUri = result.getData();
+            Settings.System.putString(getContentResolver(), Settings.System.SYNTHOS_VOLUME_PANEL_BACKGROUND_IMAGE, imageUri.toString());
+        }
     }
 
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
